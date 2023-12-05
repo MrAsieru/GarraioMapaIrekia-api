d2197651a24c0a08a795d054843b0d45fe1d27d0
MrAsieru <28875845+MrAsieru@users.noreply.github.com>
2023-12-05 10:02:17 +0100

    fix: posiciones añadir tipo linea

--- api/models/posicion.py
+++ api/models/posicion.py
@@ -2,12 +2,13 @@ from pydantic import BaseModel
 from datetime import datetime
 
 class PosicionesRequestModel(BaseModel):
-    fecha: str
-    agencias: list[str]
+  fecha: str
+  agencias: list[str]
 
 class PosicionViajeModel(BaseModel):
   idViaje: str
   idLinea: str
+  tipo: int | None = None #TODO: Quitar None
   posiciones: str
 
 class PosicionAgenciaModel(BaseModel):
